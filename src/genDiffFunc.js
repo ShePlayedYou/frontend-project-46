30ac1459da1ed78ceec4e0b444a8446912509ebd
Nastya <kota1213@mail.ru>
2025-03-09 18:11:23 +0300

    Added -f options and arguments

--- src/genDiffFunc.js
+++ src/genDiffFunc.js
@@ -5,6 +5,9 @@ const gendiff = new Command();
 gendiff
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
+  .argument('filepath1')
+  .argument('filepath2')
+  .option('-f, --format [type]', 'output format')
   .parse();
 
 export default gendiff;
